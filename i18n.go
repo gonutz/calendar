39a095b439aa3c022c2f558689be3cba0848330a
Lars <lars.scheme@gmail.com>
2019-01-23 16:05:05 +0100

    Show calendar week on Mondays

--- i18n.go
+++ i18n.go
@@ -18,15 +18,27 @@ var langNames = []string{
 	german:    "Deutsch",
 }
 
+var weekPrefix = []string{
+	englishUS: "CW %d  ",
+	englishGB: "CW %d  ",
+	german:    "KW %d  ",
+}
+
 func formatDate(t time.Time, lang int) string {
 	y, m, d := t.Date()
-	shortDay := shortDays[lang][t.Weekday()]
+	weekday := t.Weekday()
+	shortDay := shortDays[lang][weekday]
+	var week string
+	if weekday == time.Monday {
+		_, w := t.ISOWeek()
+		week = fmt.Sprintf(weekPrefix[lang], w)
+	}
 	switch lang {
 	default: // English US
-		return fmt.Sprintf("%s %02d/%02d/%04d", shortDay, m, d, y)
+		return fmt.Sprintf("%s%s %02d/%02d/%04d", week, shortDay, m, d, y)
 	case englishGB:
-		return fmt.Sprintf("%s %02d/%02d/%04d", shortDay, d, m, y)
+		return fmt.Sprintf("%s%s %02d/%02d/%04d", week, shortDay, d, m, y)
 	case german:
-		return fmt.Sprintf("%s %02d.%02d.%04d", shortDay, d, m, y)
+		return fmt.Sprintf("%s%s %02d.%02d.%04d", week, shortDay, d, m, y)
 	}
 }
